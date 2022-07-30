53a99f6bf98a7981795992c795e14100fd440b4e
Philip Persan <phillipeslash@gmail.com>
2022-07-30 12:05:38 -0300

    template finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pDrak: "#333333",
+        pBgDark: "#444444",
+        pWhite: "#F6F6F6",
+        pGray: "#E8E8E8",
+        pDarkRed: "#990100",
+        pRed: "#B90504",
+      },
+    },
   },
   plugins: [],
 };
